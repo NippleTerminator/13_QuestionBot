64e4044bfa62bc1069adfcb2b1149bb8d5d9d8b3
kevinctc <kevinctc@gmail.com>
2017-03-03 17:22:42 +0800

    add "hello" prefix response

--- QuestionBot/QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,10 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
         // TODO: Write a response 
+        let lowerQuestion = question.lowercased()
+        if lowerQuestion.hasPrefix("hello") {
+            return "Hi there!"
+        }
         return "Sorry, please repeat your questions."
     }
 }
